e97d00c9af75059ad4d8d313b96c988a10ceb4ab
marioleudo <leudomario8@gmail.com>
2023-02-27 12:23:46 -0500

    primeros precesos js (fusionando los html)

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navEmail = document.querySelector(".navbar-email");
 const menudesktop = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 navEmail.addEventListener("click", togglemenudesktop);
+menuHamIcon.addEventListener("click", togglemobileMenu);
 
 function togglemenudesktop() {
   menudesktop.classList.toggle("inactive");
+}
+
+function togglemobileMenu() {
+  mobileMenu.classList.toggle("inactive");
 }
